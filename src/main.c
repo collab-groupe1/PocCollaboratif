de1b53af5d826a719cf2bcc7400c5630da9611bd
Hyna <bylo@duck.com>
2025-10-04 23:17:38 +0200

    feat(us1): implement addition of two numbers

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello Word\n");
+int main()
+{
+    float a, b;
+    printf("*****(US1) - addition*****\n");
+
+    printf("Entrer le premier nombre (a) : ");
+    scanf("%f", &a);
+
+    printf("Entrer le deuxième nombre (b) : ");
+    scanf("%f", &b);
+
+    printf("La somme de %.2f et %.2f = %.2f\n", a, b, a + b);
+
     return 0;
 }
